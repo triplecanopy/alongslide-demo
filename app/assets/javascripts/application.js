3e2b14b14cdaebda50bb17702f9458a4d3bd0fb6
Adam Florin <adamflorin@gmail.com>
2014-06-28 16:48:58 -0700

    Add resize handler which refreshes, then re-renders after a delay

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,6 +3,7 @@
 //= require alongslide
 
 MIN_WINDOW_WIDTH = 980
+DEBOUNCE_RESIZE_MS = 250
 
 // On DOM ready, load fonts
 // 
@@ -22,20 +23,49 @@ $(document).ready(function() {
 // 
 function loadStyles() {
   Styles.prototype.doLoad(function() {
-    render()
+    init()
   })
+}
+
+// Initialize Alongslide
+// 
+function init() {
+  window.alongslide = new Alongslide({
+    source: '#content',
+    to: '#frames'
+  })
+  render()
 }
 
 // Render Alongslide
 // 
 function render() {
-  window.alongslide = new Alongslide({
-    source: '#content',
-    to: '#frames'
-  })
+  $('#content-display').css({opacity: 0.0})
   frameAspect = FixedAspect.prototype.fitFrame(MIN_WINDOW_WIDTH)
   window.alongslide.render(frameAspect, function() {
       FixedAspect.prototype.fitPanels(frameAspect)
       $('#content-display').animate({opacity: 1.0}, 150)
   })
 }
+
+// Refresh Alongslide (update scroll handlers without re-flowing all content)
+// 
+function refresh() {
+  frameAspect = FixedAspect.prototype.fitFrame(MIN_WINDOW_WIDTH)
+  window.alongslide.refresh(frameAspect)
+  FixedAspect.prototype.fitPanels(frameAspect)
+}
+
+// Resize handler
+// 
+// Refresh instantly as it's fast--and only re-render after a timeout to avoid
+// doing redundant renders.
+// 
+$(window).resize(function() {
+  refresh()
+
+  clearTimeout(window.renderTimeout)
+  window.renderTimeout = setTimeout(function() {
+    render()
+  }, DEBOUNCE_RESIZE_MS)
+})
